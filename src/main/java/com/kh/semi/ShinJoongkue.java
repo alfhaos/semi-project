ef717b6da028d5f83ec22d993d0872db2b082427
jackshin11 <jkshin9000@gmail.com>
2021-11-25 21:08:35 +0900

    신중규패키지생성!

---
+++ src/main/java/com/kh/semi/ShinJoongkue.java
@@ -0,0 +1,20 @@
+package com.kh.semi;
+
+public class ShinJoongkue {
+
+	
+	public void foo() {
+		System.out.println("ShinJoongku class 생성!!!");
+		
+	}
+
+	public void bar() {
+		System.out.println("bar 메소드 생성!!");
+		
+	}
+	
+	public void why() {
+		System.out.println("why 메소드 생성!!");
+		
+	}
+}
