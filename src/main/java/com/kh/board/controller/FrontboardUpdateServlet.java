97777a8ee1a8353250757e6760eefd99bcf2ecb8
p5na9 <sp5na92@gmail.com>
2021-12-08 14:36:48 +0900

    update, delete (dao, service작성)

--- src/main/java/com/kh/board/controller/FrontboardUpdateServlet.java
+++ src/main/java/com/kh/board/controller/FrontboardUpdateServlet.java
@@ -1,22 +1,24 @@
 package com.kh.board.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.kh.community.model.service.FreeboardService;
+import com.kh.board.model.service.FrontboardService;
+import com.kh.board.model.vo.Frontboard;
 import com.kh.community.model.vo.Freeboard;
 
 /**
  * Servlet implementation class FrontboardUpdateServlet
  */
-@WebServlet("/FrontboardUpdateServlet")
+@WebServlet("/board/frontboardUpdate")
 public class FrontboardUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private FreeboardService freeboardService = new FreeboardService();
+	private FrontboardService frontboardService = new FrontboardService();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -25,12 +27,12 @@ public class FrontboardUpdateServlet extends HttpServlet {
 		
 		int no = Integer.parseInt(request.getParameter("no"));
 		
-		Freeboard freeboard = freeboardService.selectOneFreeBoard(no);
-		System.out.println("[freeboardUpdateServlet] freeboard = " + freeboard);
+		Frontboard frontboard = frontboardService.selectOneBoard(no);
+		System.out.println("[frontboardUpdateServlet] frontboard = " + frontboard);
 		
-		request.setAttribute("freeboard", freeboard);
+		request.setAttribute("frontboard", frontboard);
 		request
-			.getRequestDispatcher("/WEB-INF/views/community/freeboardUpdate.jsp")
+			.getRequestDispatcher("/WEB-INF/views/board/frontboardUpdate.jsp")
 			.forward(request, response);
 	}
 
@@ -46,16 +48,16 @@ public class FrontboardUpdateServlet extends HttpServlet {
 		String writer = request.getParameter("writer");
 		String content = request.getParameter("content");
 		
-		Freeboard freeboard = new Freeboard(no, title, writer, content, 0, null, no);
+		Frontboard frontboard = new Frontboard(no, title, writer, content, 0, null, no);
 		
-		int result = freeboardService.updateFreeBoard(freeboard);
-		System.out.println("[FreeboardUpdateServlet] result = " + result);
+		int result = frontboardService.updateFrontBoard(frontboard);
+		System.out.println("[FrontboardUpdateServlet] result = " + result);
 		String msg = result > 0 ? "게시물 수정 성공!" : "게시물 수정 실패!";
 		
 		// 3.redirect: DML은 redirect해서 url을 변경한다.
 		// location: /mvc/board/boardList
 		request.getSession().setAttribute("msg", msg);
-		String location = request.getContextPath() + "/community/freeboardView?no=" + freeboard.getNo();
+		String location = request.getContextPath() + "/board/frontboardView?no=" + frontboard.getNo();
 		response.sendRedirect(location);
 	}
 
